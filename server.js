e6160fbf5e0060995baad2803e0657630f22c4ff
Jorge Villarreal <jv.meinecke@gmail.com>
2018-05-25 21:53:44 -0400

    ES6 and trycatch

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-var express = require('express')
-var cors = require('cors')
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var app = express()
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 
-var auth = require('./controllers/auth')
-var posts = require('./controllers/posts')
-var users = require('./controllers/users')
+const auth = require('./controllers/auth');
+const posts = require('./controllers/posts');
+const users = require('./controllers/users');
 
-mongoose.Promise = Promise
+const init = async () => {
+  try {
+    mongoose.Promise = Promise;
 
-app.use(cors())
-app.use(bodyParser.json())
+    app.use(cors());
+    app.use(bodyParser.json());
 
-var mongoUriString =
-process.env.MLAB_URI ||
-'mongodb://localhost/jvm-social' 
+    const mongoUriString =
+      process.env.MLAB_URI || 'mongodb://localhost/jvm-social';
 
+    await mongoose.connect(mongoUriString, { useMongoClient: true });
+    
+    app.use('/auth', auth.router);
+    app.use('/posts', posts);
+    app.use('/users', users);
 
-mongoose.connect(mongoUriString, { useMongoClient: true }, (err) => {
-    if (!err)
-        console.log('connected to mongo')
-})
- 
-app.use('/auth', auth.router)
-app.use('/posts',posts)
-app.use('/users',users)
+    app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-app.listen(process.env.PORT || 3000)
+init();
